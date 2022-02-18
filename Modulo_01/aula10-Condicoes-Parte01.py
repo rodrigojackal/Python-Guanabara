2a2b2fde36fa82b53c93d3936f678634fbea80d6
Rodrigo F. S. Andrade <rodrigoferreirasa@hotmail.com>
2022-02-18 20:02:24 -0300

    Inclusão do arquivo da aula 10

---
+++ Modulo_01/aula10-Condicoes-Parte01.py
@@ -0,0 +1,26 @@
+# Aula 10 – Condições (Parte 1)
+"""
+Nessa aula, vamos aprender como utilizar estruturas condicionais simples e compostas nos seus programas em Python.
+"""
+
+# Desafios
+
+"""
+Desafio 028 - Jogo da Adivinhação v.1.0: 
+
+Desafio 029 - Radar eletrônico: 
+
+Desafio 030 - Par ou Ímpar?: 
+
+Desafio 031 - Custo da Viagem: 
+
+Desafio 032 - Ano Bissexto: 
+
+Desafio 033 - Maior e menor valores:
+
+Desafio 034 - Aumentos múltiplos: 
+
+Desafio 035 - Analisando Triângulo v1.0:  
+"""
+
+
